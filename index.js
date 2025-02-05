fbe601367cc0eb0274f3f8acb3603154a2fae160
Vansh Patel <patel.vansh1607@gmail.com>
2025-02-05 17:05:54 +0300

    added details

--- index.js
+++ index.js
@@ -1,4 +1,16 @@
 console.log("HTML FORMS")
+
+let details = {
+    firstName: '',
+    lastName: '',
+    email: '',
+    phoneNumber: '',
+    password: '',
+    confirmPassword: '',
+}
+
+
+
 const firstName = document.getElementById("firstName")
 firstName.addEventListener("change", (e) =>{
     const name = e.target.value
