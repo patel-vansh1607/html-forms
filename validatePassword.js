878b4233c995a19757ff45242d892e11fd56a38c
Vansh Patel <patel.vansh1607@gmail.com>
2025-02-11 17:25:41 +0300

    added regex

--- validatePassword.js
+++ validatePassword.js
@@ -1,5 +1,5 @@
 function validatePassword(password,confirmPassword){
-    if (password===confirmPassword && password.length >= 4 && password!== "" && password.length != 0 && password.trim().length!==0){
+    if (password===confirmPassword && password.length >= 4 && password!== "" && password.length != 0 && password.trim().length!==0 && /\d/.test(password) && /[a-zA-Z]/.test(password)){
         return true;
     } else {
         return false;
