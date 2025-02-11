4b620283463d182162777740fdefaae3541424c0
Vansh Patel <patel.vansh1607@gmail.com>
2025-02-11 17:03:14 +0300

    adding assertions

--- tests/form.test.js
+++ tests/form.test.js
@@ -3,6 +3,11 @@ const validatePassword = require("../validatePassword")
 test("validate password works as expected", () =>{
     expect(validatePassword("s", "S")).toBe(false)
     expect(validatePassword("S", "S")).toBe(false)
-    // expect(validatePassword("","")).toBe(false)
+    expect(validatePassword("Password123", "Password1234")).toBe(false)
+    expect(validatePassword("SSSS", "SSSS")).toBe(true)
+    expect(validatePassword("","")).toBe(false)
+    expect(validatePassword("Pass","")).toBe(false)
+    expect(validatePassword("password1234","Password1234")).toBe(false)
+    expect(validatePassword("PaSwORd2212345678","PaSwORd2212345678")).toBe(true)
 }
 )
