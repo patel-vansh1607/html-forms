930d31902147e135d228dda249873f36508a61e5
Vansh Patel <patel.vansh1607@gmail.com>
2025-02-11 17:09:25 +0300

    added conditions

--- validatePassword.js
+++ validatePassword.js
@@ -1,5 +1,5 @@
 function validatePassword(password,confirmPassword){
-    if (password===confirmPassword && password.length >= 4){
+    if (password===confirmPassword && password.length >= 4 && password!== "" && password.length != 0 && password.trim().length!==0){
         return true;
     } else {
         return false;
